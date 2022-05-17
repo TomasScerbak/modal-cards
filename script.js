0ba0f122ad9aa69c150cab84407e5710406f0def
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-05-17 14:08:27 +0200

    grabbing DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Grabbing DOM elements
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const modalBtns = document.querySelectorAll('.show-modal');
+
+modalBtns.forEach(element => {
+    element.addEventListener('click', function() {
+        modal.classList.remove('hidden');
+    })
+})
